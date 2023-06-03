3a7e51360c01fcde533e02cf695a5fed1d7eb641
dmitriysenko <dmitriy.senko.job@gmai.com>
2023-06-03 23:33:14 +0300

    Added Task2.sql

---
+++ TestTask/Task2.sql
@@ -0,0 +1,8 @@
+-- В базе данных MS SQL Server есть продукты и категории. Одному продукту может соответствовать много категорий,
+-- в одной категории может быть много продуктов. Напишите SQL запрос для выбора всех пар
+-- «Имя продукта – Имя категории». Если у продукта нет категорий, то его имя все равно должно выводиться.
+
+SELECT p.ProductName, c.CategoryName
+FROM Products p
+         LEFT JOIN ProductCategories pc ON p.ProductID = pc.ProductID
+         LEFT JOIN Categories c ON pc.CategoryID = c.CategoryID
